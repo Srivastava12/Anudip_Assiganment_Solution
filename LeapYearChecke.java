b521ca8cfa62c36c7ed8106d4de9be56d483b67b
Shubham Kumar <91357137+Srivastava12@users.noreply.github.com>
2024-09-12 20:21:58 +0530

    Add files via upload

---
+++ LeapYearChecke.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+/***
+ * @author Shubham Kumar
+ ***/
+public class LeapYearChecke {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+
+        // Check if the year is a leap year
+        if (year % 400 == 0) {
+            System.out.println(year + " is a leap year.");
+        } else if (year % 100 == 0) {
+            System.out.println(year + " is not a leap year.");
+        } else if (year % 4 == 0) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+
+        scanner.close();
+    }
+}
