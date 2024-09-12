45b311b97747cecceedd8643dbb8836878cdda40
Shubham Kumar <91357137+Srivastava12@users.noreply.github.com>
2024-09-12 20:21:14 +0530

    Add files via upload

---
+++ StrongNumber.java
@@ -0,0 +1,44 @@
+
+import java.util.Scanner;
+
+/***
+ * @author Shubham Kumar
+ */
+public class StrongNumber {
+    // Function to calculate factorial of a digit
+    public static int factorial(int num) {
+        int fact = 1;
+        for (int i = 1; i <= num; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
+    // Function to check if a number is a Strong number
+    public static boolean isStrongNumber(int number) {
+        int originalNumber = number;
+        int sumOfFactorials = 0;
+
+        while (number > 0) {
+            int digit = number % 10;
+            sumOfFactorials += factorial(digit);
+            number /= 10;
+        }
+
+        return sumOfFactorials == originalNumber;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (isStrongNumber(number)) {
+            System.out.println(number + " is a Strong number.");
+        } else {
+            System.out.println(number + " is not a Strong number.");
+        }
+
+        scanner.close();
+    }
+}
